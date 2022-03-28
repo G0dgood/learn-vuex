53337238fd0d05210aee4746b5a8de99333cdeb4
Your Name <you@example.com>
2022-03-28 19:54:42 +0100

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,13 +6,44 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    counter:0,
+    colorCode:'blue'
   },
   mutations: {
-  },
+    decreaseCounter(state,randomNumber){
+    state.counter -= randomNumber
+    },
+    increaseCounter(state, randomNumber){
+      state.counter += randomNumber
+    //  console.log('randomNumber',randomNumber) 
+    },
+setColorCode(state,newValue){
+  state.colorCode = newValue 
+}
+},
   actions: {
+    increaseCounter({commit}){
+      // console.log('increaseCounter (action)')
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(res =>{
+        commit('increaseCounter', res.data)
+        console.log(res)
+      })
+    },
+    decreaseCounter({commit}){ 
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(res =>{
+        commit('decreaseCounter', res.data) 
+
+      })
+    },
+    setColorCode({commit}, newValue){
+      commit('setColorCode',newValue)
+    }
   },
+  getters: {
+    counterSquared(state){
+       return state.counter * state.counter
+    }
+  }, 
   modules: {
   }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <counter/>
+<counter-Squared/>
+<Buttons/>
+  
+  <input 
+  v-model="colorCode"
+  placeholder="Ender colour code" 
+  type="text"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+ import Counter from '@/components/Counter'
+ import CounterSquared from '@/components/CounterSquared'
+ import Buttons from '@/components/Buttons'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { Counter, CounterSquared ,Buttons},
+  computed:{
+    colorCode:{
+get(){
+return this.$store.state.colorCode
+},
+    set(newValue){
+this.$store.dis('setColorCode',newValue)
+    } 
+    }
+  },
+
+   
+  
+  
 }
 </script>
+
+<style>
+div {
+margin-bottom: 10px;
+}
+.counter{
+  font-size: 80px;
+}
+
+.buttons button {
+  font-size: 40px;
+  width:100px;
+  margin: 0 10px;
+}
+</style>
+
